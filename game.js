581d26d812725e30883aa489aa0580f7cad48a3d
Daniel Hildebrandt <berylcomplex@gmail.com>
2015-02-17 22:57:17 -0800

    Phaser Setup
    Added a JS file with the empty functions needed by Phaser, changed the
    index around to fit.

---
+++ game.js
@@ -0,0 +1,20 @@
+window.onload = function()
+{
+	var game = new Phaser.Game(640, 800, Phaser.AUTO, "",
+		{preload: preload, create: create, update: update});
+	
+	function preload()
+	{
+		
+	}
+	
+	function create()
+	{
+		
+	}
+	
+	function update()
+	{
+		
+	}
+}
