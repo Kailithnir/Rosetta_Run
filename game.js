7f3157800605e9f5302b27bbd14459560a5e154c
Daniel Hildebrandt <berylcomplex@gmail.com>
2015-02-20 14:53:22 -0800

    Temporary Sprites and Constants
    Added temporary small and large enemy sprites, got started with
    variables for later and a function to spawn by lane. Adjustment of
    directory names.

--- game.js
+++ game.js
@@ -1,16 +1,34 @@
 window.onload = function()
 {
-	var game = new Phaser.Game(640, 800, Phaser.AUTO, "",
+	var GAME_WIDTH = 400, GAME_HEIGHT = 600;
+	var NUM_LANES = 5, LANE_WIDTH = GAME_WIDTH / NUM_LANES, LANE_MARGIN = 5;
+	
+	var PLAYER_SIZE = 64;
+	var ENEMY_SIZE_SMALL = LANE_WIDTH - (2 * LANE_MARGIN);
+	var ENEMY_SIZE_LARGE = (2 * LANE_WIDTH) - (2 * LANE_MARGIN);
+	
+	var game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, "",
 		{preload: preload, create: create, update: update});
+	
+	var player, enemies;
+	var gameSpeed = 1, maxSpeed = 10;
+	
+	function spawnEnemy(name, lane)
+	{
+		var x = LANE_MARGIN + ((lane - 1) * LANE_WIDTH);
+		game.add.sprite(x, -(ENEMY_SIZE_LARGE), name);
+	}
 	
 	function preload()
 	{
-		
+		game.load.image("smallEnemy", "assets/Temp_Small.png");
+		game.load.image("largeEnemy", "assets/Temp_Large.png");
 	}
 	
 	function create()
 	{
-		
+		game.add.sprite(LANE_MARGIN, 0, "smallEnemy");
+		game.add.sprite((3 * LANE_MARGIN) + 70, 0, "largeEnemy");
 	}
 	
 	function update()
